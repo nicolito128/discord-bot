95126995b3e30fb5e34490d137383cb06ae28280
Nicolas <nicolito128@hotmail.com>
2019-03-04 17:21:27 +0000

    Passing parameters to the commands

--- commands.js
+++ commands.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 
 function loadCommands(message) {
+    const channel = message.channel;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
     let files = fs.readdirSync('./commands');
     for (let file of files) {
         if (!file.includes('.js')) continue;
         let req = require(`./commands/${file}`);
-        req.init(message);
+        req.init(message, command, args, channel);
     }
 }
 
