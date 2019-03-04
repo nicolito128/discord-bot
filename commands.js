2ee6b214bd0801e6a302f9c05b95659a0b031f49
Nicolas <nicolito128@hotmail.com>
2019-03-04 17:06:00 +0000

    File exceptions

--- commands.js
+++ commands.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function loadCommands(message) {
     let files = fs.readdirSync('./commands');
     for (let file of files) {
-        if (file === 'data') continue;
+        if (!file.includes('.js')) continue;
         let req = require(`./commands/${file}`);
         req.init(message);
     }
