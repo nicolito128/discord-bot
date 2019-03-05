708678b85810df49f7018cb035f4490d2032aa97
Nicolas <nicolito128@hotmail.com>
2019-03-05 22:42:09 +0000

    Add option for title and format

--- commands/pastebin.js
+++ commands/pastebin.js
@@ -3,15 +3,22 @@ const paste = require('better-pastebin');
 paste.setDevKey("devKey"); // https://pastebin.com/api
 
 const init = function(message, command, args, channel) {
-    
-    
     if (command === "paste" || command === "pastebin" || command === "newpaste") {
-        let text = args.join(' ');
-        if (!text) return channel.send('Ingresa un texto el cual escribir.');
+        let targets = args.join(" ");
+        targets = targets.split(">>");
+        
+        let text = targets[0];
+        let title = targets[1];
+        let format = targets[2];
+        
+        if (!title) title = 'Untitle';
+        if (!format) format = 'text';
+        if (!text) return channel.send('Uso: $newpaste [**contenido del paste**] >>[**titulo**] >>[**format**] - El titulo y el formato son opcionales. Ejemplo de uso: $newpaste var myApp = {}; >>My JS code >>javascript');
         paste.create({
             contents: text,
-            name: 'Untitle',
-            privacy: '0'
+            name: title,
+            privacy: '0',
+            format: format
         }, (success, data) => {
             if (success) {
                 channel.send(data);
