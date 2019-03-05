6e6692849dc14f6999a82cc8d69bc44dfcf364d6
Nicolas <nicolito128@hotmail.com>
2019-03-05 18:48:37 +0000

    Update

--- commands.js
+++ commands.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 function loadCommands(message) {
-    const channel = message.channel;
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    let status = false;
+    let channel = message.channel,
+        args = message.content.slice(config.prefix.length).trim().split(/ +/g),
+        command = args.shift().toLowerCase(),
+        status = false;
 
     const files = fs.readdirSync('./commands');
     for (let file of files) {
@@ -12,8 +12,8 @@ function loadCommands(message) {
         let req = require(`./commands/${file}`);
         
         if (!req.help.cmds) return new Error('The list of "cmds" needed to execute the command was not found');
-        
         let cmds = req.help.cmds;
+        
         for (let cmd of cmds) {
             if (cmd === command) {
                 req.init(message, command, args, channel);
