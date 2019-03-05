46a82aff7a1abf91ac68acaeaee84de198391e15
Nicolas <nicolito128@hotmail.com>
2019-03-05 01:46:01 +0000

    Updated loadCommands

--- commands.js
+++ commands.js
@@ -5,11 +5,21 @@ function loadCommands(message) {
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    let files = fs.readdirSync('./commands');
+    const files = fs.readdirSync('./commands');
     for (let file of files) {
         if (!file.includes('.js')) continue;
         let req = require(`./commands/${file}`);
-        req.init(message, command, args, channel);
+        
+        if (!req.help.cmds) return new Error('The list of "cmds" needed to execute the command was not found');
+        
+        let cmds = req.help.cmds;
+        for (let cmd of cmds) {
+            if (cmd === command) {
+                req.init(message, command, args, channel);
+                // console.log(req.help.name + ' loaded!');
+                break;
+            } else continue;
+        }
     }
 }
 
