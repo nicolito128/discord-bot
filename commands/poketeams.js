b7894c63a8aee234377c4d0687a39e9ed4f0248b
Nicolas <nicolito128@hotmail.com>
2019-03-05 20:37:46 +0000

    Update

--- commands/poketeams.js
+++ commands/poketeams.js
@@ -4,12 +4,12 @@ const init = function(message, command, args, channel) {
     let teams = {};
     teams = JSON.parse(fs.readFileSync(__dirname + '/../data/teams.json'));
     
-    if (command === 'teamadd') {
+    if (command === 'addteam') {
         let targets = args.join(" ");
         targets = targets.split(" ");
         
         if (!targets[0] || targets[0] === 'help') {
-            return channel.send('Uso: $teamadd [**tier**] [**team**]');
+            return channel.send('Uso: $addteam [**tier**] [**team**]');
         }
         
         let tier = targets[0];
@@ -26,7 +26,7 @@ const init = function(message, command, args, channel) {
         channel.send('Equipo añadido.');
     }
     
-    if (command === 'ou' || command === 'ubers' || command === 'uu') {
+    if (command === 'ou' || command === 'ubers' || command === 'uu' || command === 'pu' || command === 'nu' ||command === 'lc') {
         let team = teams[command];
         if(team === undefined) return channel.send('No hay equipos de esta tier.');
         let random = Math.round(Math.random() * (team.length - 1));
@@ -40,6 +40,6 @@ module.exports = {
     init,
     help: {
         name: 'Pokémon Teams',
-        cmds: ['ou', 'ubers', 'uu', 'teamadd']
+        cmds: ['ou', 'ubers', 'uu', 'pu', 'nu', 'lc', 'addteam']
     }
 };
