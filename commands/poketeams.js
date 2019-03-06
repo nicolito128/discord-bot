639f86375e8973ab664763ddb084583eb172cfaa
Nicolas <nicolito128@hotmail.com>
2019-03-06 17:31:44 +0000

    Update

--- commands/poketeams.js
+++ commands/poketeams.js
@@ -1,39 +1,45 @@
 const fs = require('fs');
+const validTiers = ['ou', 'ubers'];
 
 const init = function(message, command, args, channel) {
-    let teams = {};
-    teams = JSON.parse(fs.readFileSync(__dirname + '/../data/teams.json'));
-    
-    if (command === 'addteam') {
-        let targets = args.join(" ");
-        targets = targets.split(" ");
-        
-        if (!targets[0] || targets[0] === 'help') {
-            return channel.send('Uso: $addteam [**tier**] [**team**]');
-        }
-        
-        let tier = targets[0];
-        let team = targets[1];
-        
-        if(!tier) return channel.send('Especifica una tier.');
-        if(!team) return channel.send('Especifica un team.');
-        if (tier === 'ou' || tier === 'ubers' || tier === 'uu' || tier === 'pu' || tier === 'nu' || tier === 'lc') {
-            if(!teams[tier]) teams[tier] = [];
-            teams[tier].push(team);
-            teams = JSON.stringify(teams);
-            fs.writeFile(__dirname + '/../data/teams.json', teams);
-            
-            return channel.send('Equipo añadido.');
-        } else return channel.send('Especifica una tier válida.');
-    }
-    
-    if (command === 'ou' || command === 'ubers' || command === 'uu' || command === 'pu' || command === 'nu' || command === 'lc') {
-        let team = teams[command];
-        if(team === undefined) return channel.send('No hay equipos de esta tier.');
-        let random = Math.round(Math.random() * (team.length - 1));
-        
-        channel.send(`Random team ${command.toUpperCase()} for you: ${team[random]}`);
-    }
+	let teams = {};
+	teams = JSON.parse(fs.readFileSync(__dirname + '/../data/teams.json'));
+	
+	if (command === 'addteam') {
+		let targets = args.join(" ");
+		targets = targets.split(" ");
+		
+		if (!targets[0] || targets[0] === 'help') {
+			return channel.send('Uso: $addteam [**tier**] [**team**]');
+		}
+		
+		let tier = targets[0].toLowerCase();
+		let team = targets[1];
+		
+		if (!tier) return channel.send('Especifica una tier.');
+		if (!team) return channel.send('Especifica un team.');
+		
+		for (let t in validTiers) {
+			if (tier === validTiers[t]) {
+				if (!teams[tier]) teams[tier] = [];
+				
+				teams[tier].push(team);
+				teams = JSON.stringify(teams);
+				fs.writeFile(__dirname + '/../data/teams.json', teams);
+				
+				return channel.send('Equipo añadido.');
+			} else return channel.send('Especifica una tier válida.');
+		}
+	}
+	
+	for (let t in validTiers) {
+		if (command === validTiers[t]) {
+			let team = teams[command];
+			if (team === undefined) return channel.send('No hay equipos de esta tier.');
+			let random = Math.round(Math.random() * (team.length - 1));
+			channel.send(`Random team ${command.toUpperCase()} for you: ${team[random]}`);
+		}
+	}
 };
 
 module.exports = {
