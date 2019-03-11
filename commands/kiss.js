33e8813c174bd260734b7b6bd83b8f7c25204586
Nicolás <nicolito128@hotmail.com>
2019-03-11 11:25:19 -0300

    Update

--- commands/kiss.js
+++ commands/kiss.js
@@ -1,8 +1,20 @@
 const kisses = [
-    'https://media.giphy.com/media/hgcqkgBLCbwl2/giphy.gif',
-    'https://media.giphy.com/media/HKQZgx0FAipPO/giphy.gif',
-    'https://media.giphy.com/media/5GdhgaBpA3oCA/giphy.gif',
-    'https://media.giphy.com/media/CE1sk31EVmjNS/giphy.gif'
+    {
+        link: 'https://media.giphy.com/media/hgcqkgBLCbwl2/giphy.gif',
+        color: 0x5363B0
+    },
+    {
+        link: 'https://media.giphy.com/media/HKQZgx0FAipPO/giphy.gif',
+        color: 0x3A627A
+    },
+    {
+        link: 'https://media.giphy.com/media/5GdhgaBpA3oCA/giphy.gif',
+        color: 0xD4AA32
+    },
+    {
+        link: 'https://media.giphy.com/media/CE1sk31EVmjNS/giphy.gif',
+        color: 0xCA3131
+    }
 ];
 
 function init(message, user, command, args) {
@@ -12,8 +24,8 @@ function init(message, user, command, args) {
     if (message.mentions.users.size < 1) return message.reply('You have to mention someone.');
     
     let embed = new Discord.RichEmbed()
-        .setColor(0x00AE86)
-        .setImage(randomKiss);
+        .setColor(randomKiss.color)
+        .setImage(randomKiss.link);
     message.channel.send(`**${user} kissed ${targetUser}**!!`);
     return message.channel.send(embed);
 }
