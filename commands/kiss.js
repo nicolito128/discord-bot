b6a7973190347c0d75191d54268090e5e34cafea
Nicolas <nicolito128@hotmail.com>
2019-03-13 18:24:34 +0000

    Update

--- commands/kiss.js
+++ commands/kiss.js
@@ -1,33 +1,4 @@
-const kisses = [
-    {
-        link: 'https://media.giphy.com/media/hgcqkgBLCbwl2/giphy.gif',
-        color: 0x5363B0
-    },
-    {
-        link: 'https://media.giphy.com/media/HKQZgx0FAipPO/giphy.gif',
-        color: 0x3A627A
-    },
-    {
-        link: 'https://media.giphy.com/media/5GdhgaBpA3oCA/giphy.gif',
-        color: 0xD4AA32
-    },
-    {
-        link: 'https://media.giphy.com/media/CE1sk31EVmjNS/giphy.gif',
-        color: 0xCA3131
-    },
-    {
-        link: 'https://media.giphy.com/media/3o72FiXBdWRy3aZHJm/giphy.gif',
-        color: 0x73DC4E
-    },
-    {
-        link: 'https://media.giphy.com/media/G3va31oEEnIkM/giphy.gif',
-        color: 0xF899D1
-    },
-    {
-        link: 'https://media.giphy.com/media/bm2O3nXTcKJeU/giphy.gif',
-        color: 0xB5B8B9
-    }
-];
+const kisses = require('../data/kisses.js');
 
 function init(message, user, command, args) {
     const targetUser = message.mentions.users.first();
